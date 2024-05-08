b2a667c281bdaf320eb1cd6eb5e8cc861091d5be
EC2 Default User <ec2-user@ip-172-31-18-92.ap-northeast-1.compute.internal>
2024-05-08 12:52:54 +0000

    データベース作成

---
+++ db/migrate/20240508114444_create_processimages.rb
@@ -0,0 +1,13 @@
+class CreateProcessimages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :processimages do |t|
+
+      t.integer :post_id
+      t.integer :user_id
+      t.string :text
+
+
+      t.timestamps
+    end
+  end
+end
