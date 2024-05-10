2d2da7863d643e9c7af7b2b50c5b508f8d97064c
m-ishina9922 <mvisn.star9292@outlook.jp>
2024-05-10 04:53:59 +0000

    管理者認証機能準備2

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
-   before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resourced)
     about_path
@@ -18,7 +19,7 @@ class Admin::SessionsController < Devise::SessionsController
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
 
   end
-  # before_action :configure_sign_in_params, only: [:create]
+
 
   # GET /resource/sign_in
   # def new
