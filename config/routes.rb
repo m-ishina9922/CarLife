f9040f17e7a010493280a2f77faebd1fe1dbacd6
m-ishina9922 <mvisn.star9292@outlook.jp>
2024-05-10 04:23:58 +0000

    管理者認証機能準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  devise_for :admins, controllers: {
+    registrations: "admins/registrations",
+    sessions: "admins/sessions",
+    passwords: "admins/passwords"
+  }
+
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions",
+    passwords: "users/passwords"
+  }
+
+
   get 'homes/top'
+  root to: 'homes#top'
+  get 'homes/about', to: 'homes#about', as: :about
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
