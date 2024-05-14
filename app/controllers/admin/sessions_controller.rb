0cf2d86f79dc2411925663c3f47f16d3c4c28fb8
m-ishina9922 <mvisn.star9292@outlook.jp>
2024-05-14 05:36:42 +0000

    ゲストログイン機能作成

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  layout 'admin'
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,37 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:new, :create, :show, :index]
-  resources :users, only: [:show, :edit, :update]
 
+
+ #管理者機能
   devise_for :admin, controllers: {
     registrations: "admin/registrations",
     sessions: "admin/sessions",
     passwords: "admin/passwords"
   }
 
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+
+  #ユーザー機能
   devise_for :user, controllers: {
     registrations: "user/registrations",
     sessions: "user/sessions",
     passwords: "user/passwords"
   }
+
+  resources :users, only: [:show, :edit, :update]
+
+
+  #投稿機能
+  resources :posts, only: [:new, :create, :show, :index]
+
+  #ゲストログイン機能
+  devise_scope :user do
+  post 'users/guest_sign_in', to: 'user/sessions#guest_sign_in'
+  end
+
+
 
 
   get 'homes/top'
