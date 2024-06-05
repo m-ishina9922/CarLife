1e00b9bd93da0d2db061c2a0ef676dd714442e18
m-ishina9922 <mvisn.star9292@outlook.jp>
2024-06-05 12:13:11 +0000

    画像複数投稿no_image

--- app/forms/posts_form.rb
+++ app/forms/posts_form.rb
@@ -6,7 +6,7 @@ class PostsForm
   #ActiveRecordのカラムのような属性を加えられるようにする
 
   #インスタンス変数を使えるようにする
-  attr_accessor :name, :post_id, :tag_id, :images
+  attr_accessor :name, :post_id, :tag_id, :image
 
 
   #posts
@@ -57,9 +57,16 @@ class PostsForm
    post =Post.create(user_id: user_id, title: title, reference_site: reference_site, products_used: products_used)
    Processimage.create(user_id: user_id, text1: text1, text2: text2, text3: text3, text4: text4, text5: text5, text6: text6, text7: text7, text8: text8, text9: text9, text10: text10, post_id: post.id)
    tag = Tag.create(name: name)
-   post.images.attach(image1, image2, image3, image4, image5, image6, image7, image8, image9, image10)
-   post.save
-   
+   post.image1.attach(image1)
+   post.image2.attach(image2)
+   post.image3.attach(image3)
+   post.image4.attach(image4)
+   post.image5.attach(image5)
+   post.image6.attach(image6)
+   post.image7.attach(image7)
+   post.image8.attach(image8)
+   post.image9.attach(image9)
+   post.image10.attach(image10)
 
 
    #入力されたタグを空白で区切って配列化する
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,16 @@
 class Post < ApplicationRecord
   #画像データを保存できるようにする
-  has_many_attached :images
+  has_one_attached :image1
+  has_one_attached :image2
+  has_one_attached :image3
+  has_one_attached :image4
+  has_one_attached :image5
+  has_one_attached :image6
+  has_one_attached :image7
+  has_one_attached :image8
+  has_one_attached :image9
+  has_one_attached :image10
+  
 
   belongs_to :user
   has_many :processimages,  dependent: :destroy
