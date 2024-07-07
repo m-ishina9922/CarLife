e8b7e39c1e52c638fc785f86b4e6b49969638bab
m-ishina9922 <mvisn.star9292@outlook.jp>
2024-07-07 12:39:06 +0000

    修正

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module CarLife
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload_paths << Rails.root.join("forms")
   end
 end
